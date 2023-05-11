ee1103f060d734fb8340ebcae2573bea63fa6844
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2023-05-10 23:21:15 -0300

    feat!: typeorm and postgres configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 5432,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    synchronize: true, // Usado apenas para demonstração do desafio
+    entities: [__dirname + "/**/*.entity{.ts,.js}"],
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
